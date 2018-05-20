7c02d9210f425a01670590a3cd3e9a2a46352f69
lucioPintanel <lm.pintanel@gmail.com>
2018-05-19 21:44:50 -0300

    subject line
    what happened
    Melhorado a alteração.
    
    [ticket: X]

--- app/admin/routesFuncionarios.py
+++ app/admin/routesFuncionarios.py
@@ -8,4 +8,8 @@ def funcionarios():
 	form = FuncionarioForm()
 	results = []
 	table = Results(results)
+	form.nome.data='Lucio'
+	form.matricula.data=896
+	table.nome.data='Felipe'
+	table.matr.data=123
 	return render_template('admin/funcionarios.html', form=form, table=table)

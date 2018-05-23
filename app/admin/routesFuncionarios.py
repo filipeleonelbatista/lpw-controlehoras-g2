5c02524f2d0a2ac2af72b69bdfc5a4b2a3a6df6f
lucioPintanel <lm.pintanel@gmail.com>
2018-05-22 21:48:46 -0300

    subject line
    what happened
    - Add funcionarios.
    
    [ticket: X]

--- app/admin/routesFuncionarios.py
+++ app/admin/routesFuncionarios.py
@@ -1,9 +1,9 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_table import Table
-from .formsFuncionarios import FuncionarioForm, Results
+from .formsFuncionarios import FuncionarioForm
 from . import admin
-from app import db
 from app.models import User
+from app import db
 
 @admin.route('/admin/funcionarios', methods=['GET', 'POST'])
 def funcionarios():
@@ -11,8 +11,10 @@ def funcionarios():
 	if form.validate_on_submit() and form.salvar.data:
 		user = User(matricula=form.matricula.data, username=form.nome.data, password=form.password.data, is_admin=form.admin.data)
 		print('User: [%s]' % user.username)
-    	#db.session.add(user)
-    	#db.session.commit()
+		print('password_hash: [%s]' % user.password_hash)
+		print('is_admin: [%s]' % user.is_admin)
+		db.session.add(user)
+		db.session.commit()
     	    	
 	listTable=User.query.all()	
 	return render_template('admin/funcionarios.html', form=form, listTable=listTable)

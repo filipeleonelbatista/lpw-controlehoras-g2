d82f883fd297b9e1a34396955ed57fd110e7b5bd
lucioPintanel <lm.pintanel@gmail.com>
2018-06-02 23:18:59 -0300

    subject line
    what happened
    - Corrigido problema que tinha update.
    
    [ticket: X]

--- app/admin/formsVinculacao.py
+++ app/admin/formsVinculacao.py
@@ -1,17 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SelectField, BooleanField
+from wtforms import IntegerField, SelectField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length
-from flask_table import Table, Col, LinkCol
-
-class Results(Table):
-    id = Col('Id', show=False)
-    codFunc = Col('CodFunc')
-    descricao = Col('Descrição')
-    codProj = Col('Projeto')
-    edit = LinkCol('Edit', 'edit', url_kwargs=dict(id='id'))
 
 class VincForm(FlaskForm):
-    codProj = IntegerField('Codigo Funcionario x Projeto')
+    codVinc = IntegerField('Codigo Funcionario x Projeto')
     selectFunc = SelectField(u'Selecione o Funcionario', choices=[('funcionario', 'Funcionarios')])
     selectProj = SelectField(u'Selecione o Projeto', choices=[('projeto', 'Projetos')])
     coordenador = BooleanField('Coordenador')
+    salvar = SubmitField('Cadastrar')
+    cancelar = SubmitField('Cancelar')
--- app/admin/routesVinculacao.py
+++ app/admin/routesVinculacao.py
@@ -1,11 +1,50 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_table import Table
-from .formsVinculacao import VincForm, Results
+from .formsVinculacao import VincForm
 from . import admin
+from app import db
+from app.models import User, Project, Binding
 
 @admin.route('/admin/vinculacao', methods=['GET', 'POST'])
 def vinculacao():
 	form = VincForm()
-	results = []
-	table = Results(results)
-	return render_template('admin/vinculacao.html', form=form, table=table)
+	form.selectFunc.choices = [(user.id, user.username) for user in User.getAllUsers()]
+	form.selectProj.choices = [(project.id, project.nameProject) for project in Project.getAllProject()]
+
+	if request.method == 'POST' and form.salvar.id == "salvar":
+		try:			
+			binding = Binding(
+				idBinding=form.codVinc.data, 
+				project_id=form.selectProj.data,
+				users_id=form.selectFunc.data,
+				is_coord=form.coordenador.data
+				)
+			db.session.add(binding)
+			db.session.commit()
+			flash('Registrado com sucesso', 'success')
+		except:
+			db.session.rollback()
+			flash('Registro falhou na adição', 'danger')
+
+	elif request.method == 'GET' and request.args.get('delete'):
+		try:
+			binding = Binding.query.filter_by(idBinding=request.args.get('delete')).first_or_404()
+			db.session.delete(binding)
+			db.session.commit()
+			flash('Registro apagado com sucesso', 'danger')
+		except:
+			db.session.rollback()
+			flash('Registro falhou em apagar', 'danger')
+
+	# elif request.method == 'GET' and request.args.get('update'):
+	# 	binding = Binding.query.filter_by(idBinding=request.args.get('update')).first_or_404()
+	# 	form.codProj.data = binding.idBinding
+	# 	form.nomeProj.data = binding.nameProject	
+	# 	#form.selectClient.choices = [(client.id, client.nameEmpresa) for client in Client.getAllClient()]
+	# 	client=Client.getClientID(binding.client_id)
+	# 	form.selectClient.choices = [(client.id, client.nameEmpresa)]
+	# 	form.descrProj.data = binding.descricao
+	# 	return render_template('admin/editProject.html',form=form, action='projectUpdate')
+	
+	listTable=Binding.query.all()
+	return render_template('admin/vinculacao.html', form=form, listTable=listTable)

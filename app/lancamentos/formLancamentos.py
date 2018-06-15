17bf80625d22ad0dc4329f9704fe8ac6f34a73f9
lucioPintanel <lm.pintanel@gmail.com>
2018-06-14 22:03:56 -0300

    - Feito a adição de dados em laçamentos. -->Falta colocar as data de forma correta.
    [ticket: X]

--- app/lancamentos/formLancamentos.py
+++ app/lancamentos/formLancamentos.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SelectField, DateField, DateTimeField, SubmitField
+from wtforms import TextAreaField, SelectField, DateField, TimeField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class lancamentoForm(FlaskForm):
     selectProjeto = SelectField(u'project', validators=[DataRequired()])
-    dtInicio = DateField('Data de Inicio')
-    hrInicio = DateTimeField('Hora de Inicio')
+    dtInicio = DateField('Data de Inicio', format='%Y-%m-%d')
+    hrInicio = TimeField('Hora de Inicio')
     dtFim = DateField('Data de Termino')
-    hrFim = DateTimeField('Data de Termino')
+    hrFim = TimeField('Data de Termino')
     selectAtividade = SelectField(u'project', validators=[DataRequired()])
     descricao = TextAreaField('Descrição do trabalho realizado...')
     gravar = SubmitField('Salvar')
--- app/lancamentos/routes.py
+++ app/lancamentos/routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_required
 from . import lancamentos
@@ -5,25 +6,23 @@ from .formLancamentos import lancamentoForm
 from app import db
 from app.models import Task, Project, Lancamento
 
+def convert(t):
+    date_format = '%d/%m/%Y'
+    convert_to_format = '%Y-%m-%d'
+    return datetime.strptime(t, date_format).date().strftime(convert_to_format)
+
 @lancamentos.route('/lancamentos', methods=['GET', 'POST'])
 @login_required
 def lancamentos():
 	form = lancamentoForm()
 	form.selectProjeto.choices = [(project.id, project.nameProject) for project in Project.getAllProject()]
 	form.selectAtividade.choices = [(task.codTask, task.descricao) for task in Task.getAllTask()]
-	if request.method == 'POST' and form.iniciar.data == True:
+	if request.method == 'POST' and form.gravar.data == True:
 		print("depois post")
 		try:
 			if form.validacao(form):
 				flash('Falta preenchar um campo!', 'danger')
 			else:
-				print(form.selectProjeto.data)
-				print(form.dtInicio.data)
-				print(form.hrInicio.data)
-				print(form.dtFim.data)
-				print(form.hrFim.data)
-				print(form.selectAtividade.data)
-				print(form.descricao.data)
 				lancamento = Lancamento(
 					project_id=form.selectProjeto.data,
 					dtInic=form.dtInicio.data,
@@ -33,7 +32,6 @@ def lancamentos():
 					task_id=form.selectAtividade.data,
 					descricao=form.descricao.data
 					)
-				print("commit")
 				db.session.add(lancamento)
 				db.session.commit()
 				flash('Registrado com sucesso', 'success')
@@ -46,8 +44,8 @@ def lancamentos():
 			if request.args.get('delete') == '':
 				flash('Falta a matricula!', 'danger')
 			else:				
-				db.session.delete(user)
-				db.session.commit()
+				# db.session.delete(user)
+				# db.session.commit()
 				flash('Registro apagado com sucesso', 'danger')
 		except:
 			db.session.rollback()

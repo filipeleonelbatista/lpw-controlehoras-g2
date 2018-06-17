3d2a19965d068f284e0c73f7a7353959bc61c346
modernfunkboss <filipe.x@live.com>
2018-06-17 20:53:02 -0300

    comentado propriedades da tabela

--- app/static/js-contents.js
+++ app/static/js-contents.js
@@ -20,6 +20,10 @@ window.onclick = function(event) {
 } */
 $(document).ready( function () {
     $('#table_id').DataTable({
+    // Desabilitar
+    //pesquisa                     "searching":false,
+    //amostragem e paginas         "paging":   false,
+    //informações de amostragem    "info":     false,
     "language":{
                 "sEmptyTable": "Nenhum registro encontrado",
                 "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",

6d41741399bc72c96194c56bfb31f336f4100362
modernfunkboss <filipe.x@live.com>
2018-06-20 11:07:26 -0300

    alteração em relatorios

--- app/static/js-contents.js
+++ app/static/js-contents.js
@@ -18,12 +18,49 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 } */
+$(document).ready(function(){
+    $("#htTime").mask("99:99");
+});
+$(document).ready(function(){
+    $("#heTime").mask("99:99");
+});
+$(document).ready(function(){
+    $("#hbhTIME").mask("99:99");
+});
+ $(document).ready( function () {
+    $('#table_horasProjeto').DataTable({
+
+"searching":false,
+"paging":   false,
+"info":     false,
+    "language":{
+                "sEmptyTable": "Nenhum registro encontrado",
+                "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+                "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+                "sInfoPostFix": "",
+                "sInfoThousands": ".",
+                "sLengthMenu": "_MENU_ resultados por página",
+                "sLoadingRecords": "Carregando...",
+                "sProcessing": "Processando...",
+                "sZeroRecords": "Nenhum registro encontrado",
+                "sSearch": "Pesquisar por Texto:",
+                "oPaginate": {
+                    "sNext": "Próximo",
+                    "sPrevious": "Anterior",
+                    "sFirst": "Primeiro",
+                    "sLast": "Último"
+                },
+                "oAria": {
+                    "sSortAscending": ": Ordenar colunas de forma ascendente",
+                    "sSortDescending": ": Ordenar colunas de forma descendente"
+                }
+            }
+    });
+} );
 $(document).ready( function () {
     $('#table_id').DataTable({
-    // Desabilitar
-    //pesquisa                     "searching":false,
-    //amostragem e paginas         "paging":   false,
-    //informações de amostragem    "info":     false,
+
     "language":{
                 "sEmptyTable": "Nenhum registro encontrado",
                 "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",

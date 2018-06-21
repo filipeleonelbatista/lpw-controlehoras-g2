57e769fd86e6b970fc65eb1724aa30b4b51d8586
modernfunkboss <filipe.x@live.com>
2018-06-21 11:12:52 -0300

    lixo

--- app/home/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, redirect, request, url_for, flash
-from .forms import HomeForm
-from . import home
-
-@home.route('/home', methods=['GET', 'POST'])
-def home():
-	form = HomeForm()
-	return render_template('home/home.html', form=form)

bab488434caa75f655a02caa568ab437d14c4203
modernfunkboss <filipe.x@live.com>
2018-06-28 22:15:06 -0300

    limpando

--- app/dashaboard/routeshxp.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, redirect, request, url_for, flash
-from .forms import RelatForm, Results
-from . import dashaboard
-
-@dashaboard.route('/dashaboard/horasxprojeto', methods=['GET', 'POST'])
-def horasxprojeto():
-	return render_template('dashaboard/hxp.html')
